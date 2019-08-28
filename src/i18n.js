33d5c5a993f5b28e411992b58243be66f3c1db2a
Heinonen Ava R <avaheino@lx8-500-109.pc.helsinki.fi>
2019-08-28 11:50:04 +0300

    locales folder fix

---
+++ src/i18n.js
@@ -0,0 +1,33 @@
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
+import courseSettings from "../course-settings"
+import commonEN from "./locales/common/en"
+import pointsBalloonEN from "./locales/pointsBalloon/en"
+import userEN from "./locales/user/en"
+import commonFI from "./locales/common/fi"
+import pointsBalloonFI from "./locales/pointsBalloon/fi"
+import userFI from "./locales/user/fi"
+
+const resources = {
+  en: {
+    common: commonEN,
+    "points-balloon": pointsBalloonEN,
+    user: userEN,
+  },
+  fi: {
+    common: commonFI,
+    "points-balloon": pointsBalloonFI,
+    user: userFI,
+  },
+}
+
+i18n.use(initReactI18next).init({
+  resources,
+  ns: ["common", "user", "points-balloon"],
+  defaultNS: "common",
+  react: {
+    wait: true,
+  },
+  lng: courseSettings.language,
+})
+export default i18n
