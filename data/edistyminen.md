01f75affb956848f0b04a86407948d533aea634b
Henrik Nygren <nygrenh@gmail.com>
2019-10-30 13:22:08 +0200

    Add new points visualization

---
+++ data/edistyminen.md
@@ -0,0 +1,10 @@
+---
+path: '/progress'
+title: 'Pisteet ja edistyminen'
+hidden: false
+information_page: true
+banner: false
+---
+
+
+<points></points>
--- src/templates/Layout.js
+++ src/templates/Layout.js
@@ -9,6 +9,8 @@ import * as store from "store"
 import Pheromones from "../util/pheromones"
 import styled from "styled-components"
 import courseMetaData from "../../course-metadata.json"
+import ApolloClient from "apollo-boost"
+import { ApolloProvider } from "@apollo/react-hooks"
 
 import "./reboot.css"
 import "./theme.css"
@@ -20,7 +22,7 @@ import "typeface-roboto-mono"
 import "@fortawesome/fontawesome-svg-core/styles.css"
 
 import { config as fontAwesomeConfig } from "@fortawesome/fontawesome-svg-core"
-import { canDoResearch } from "../services/moocfi"
+import { canDoResearch, accessToken } from "../services/moocfi"
 import Footer from "../components/Footer"
 import PointsBalloon from "../components/PointsBalloon"
 import {
@@ -32,6 +34,21 @@ import {
 import withSimpleErrorBoundary from "../util/withSimpleErrorBoundary"
 
 fontAwesomeConfig.autoAddCss = false
+
+const apolloClient = new ApolloClient({
+  uri: "https://www.mooc.fi/api",
+  request: async operation => {
+    const token = accessToken()
+    if (!token) {
+      return
+    }
+    operation.setContext({
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+  },
+})
 
 const layoutQuery = graphql`
   query {
@@ -108,48 +125,50 @@ class Layout extends React.Component {
 
     return (
       <Fragment>
-        <StaticQuery
-          query={layoutQuery}
-          render={data => {
-            const siteTitle = data.title.siteMetadata.title
-            return (
-              <Wrapper mobileMenuOpen={this.state.mobileMenuOpen}>
-                <Helmet
-                  defaultTitle={siteTitle}
-                  titleTemplate={`%s - ${siteTitle}`}
-                  meta={[
-                    {
-                      name: "description",
-                      content:
-                        "Helsingin yliopiston kaikille avoin ja ilmainen ohjelmoinnin perusteet opettava verkkokurssi. Kurssilla perehdytään nykyaikaisen ohjelmoinnin perusideoihin sekä ohjelmoinnissa käytettävien työvälineiden lisäksi algoritmien laatimiseen. Kurssille osallistuminen ei vaadi ennakkotietoja ohjelmoinnista.",
-                    },
-                    {
-                      name: "keywords",
-                      content:
-                        "ohjelmointi, java, programming, CS1, MOOC, 2019, ohjelmointikurssi, avoin, ilmainen, helsingin yliopisto",
-                    },
-                  ]}
-                />
-                <Sidebar
-                  mobileMenuOpen={this.state.mobileMenuOpen}
-                  toggleMobileMenu={this.toggleMobileMenu}
-                />
-                <SidebarPush>
-                  <TopBar />
-                  <ContentArea mobileMenuOpen={this.state.mobileMenuOpen}>
-                    {children}
-                  </ContentArea>
-                  <PointsBalloon />
-                  <Footer />
-                </SidebarPush>
-              </Wrapper>
-            )
-          }}
-        />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(courseMetaData) }}
-        />
+        <ApolloProvider client={apolloClient}>
+          <StaticQuery
+            query={layoutQuery}
+            render={data => {
+              const siteTitle = data.title.siteMetadata.title
+              return (
+                <Wrapper mobileMenuOpen={this.state.mobileMenuOpen}>
+                  <Helmet
+                    defaultTitle={siteTitle}
+                    titleTemplate={`%s - ${siteTitle}`}
+                    meta={[
+                      {
+                        name: "description",
+                        content:
+                          "Helsingin yliopiston kaikille avoin ja ilmainen ohjelmoinnin perusteet opettava verkkokurssi. Kurssilla perehdytään nykyaikaisen ohjelmoinnin perusideoihin sekä ohjelmoinnissa käytettävien työvälineiden lisäksi algoritmien laatimiseen. Kurssille osallistuminen ei vaadi ennakkotietoja ohjelmoinnista.",
+                      },
+                      {
+                        name: "keywords",
+                        content:
+                          "ohjelmointi, java, programming, CS1, MOOC, 2019, ohjelmointikurssi, avoin, ilmainen, helsingin yliopisto",
+                      },
+                    ]}
+                  />
+                  <Sidebar
+                    mobileMenuOpen={this.state.mobileMenuOpen}
+                    toggleMobileMenu={this.toggleMobileMenu}
+                  />
+                  <SidebarPush>
+                    <TopBar />
+                    <ContentArea mobileMenuOpen={this.state.mobileMenuOpen}>
+                      {children}
+                    </ContentArea>
+                    <PointsBalloon />
+                    <Footer />
+                  </SidebarPush>
+                </Wrapper>
+              )
+            }}
+          />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(courseMetaData) }}
+          />
+        </ApolloProvider>
       </Fragment>
     )
   }
